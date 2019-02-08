46f6be25a117e1f6cb150e8e26aeda27161d4ccb
amie phipps <amiejoyphipps@gmail.com>
2019-02-08 16:54:04 -0500

    udpates

--- test/testUtils.js
+++ test/testUtils.js
@@ -5,7 +5,6 @@ export const findByTestAttr = (wrapper, val) => {
   return wrapper.find(`[data-test="${val}"]`)
 }
 
-
 export const checkProps = (component, conformingProps) => {
   const propError = checkPropTypes(
     component.propTypes,
