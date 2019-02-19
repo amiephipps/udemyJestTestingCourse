7796bd1ea04859a2dcf27b1de79d5a23e93bd2fd
amie phipps <amiejoyphipps@gmail.com>
2019-02-19 09:26:33 -0500

    added input component

---
+++ src/input.test.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import { findByAttr } from '../test/testUtils'
+import Input from './Input'
+
+describe('render', () => {
+  describe('the word has not been guessed', () => {
+    it('renders the component without error', () => {
+
+    })
+
+    it('renders the input box', () => {
+
+    })
+
+    it('renders the submit button', () => {
+
+    })
+  })
+
+  describe('the word has been guessed', () => {
+    it('renders the component without error', () => {
+
+    })
+
+    it('does not render the input box', () => {
+
+    })
+
+    it('does not render the submit button', () => {
+
+    })
+  })
+})
+
+describe('update state', () => {
+
+})
